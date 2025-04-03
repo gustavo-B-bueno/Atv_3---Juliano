927e9227b7701dcc5fb653e18c3474b97e87f8c4
Gustavo Basso Bueno <gustavocw10@gmail.com>
2025-04-03 20:29:22 -0300

    primeiro commit

---
+++ ProgramacaoIV.Venda.Api/Entidades/Vendedor.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProgramacaoIV.Venda.Api.Entidades
+{
+    public sealed class Vendedor : AbstractEntity
+    {
+        [Required]
+        public string Nome { get; set; } = string.Empty;
+
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+    }
+}
---
+++ ProgramacaoIV.Venda.Api/Map/VendedorMap.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProgramacaoIV.Venda.Api.Entidades;
+
+namespace ProgramacaoIV.Venda.Api.Map
+{
+    public class VendedorMap : AbstractEntidadeMap<Vendedor>
+    {
+        public override void Configure(EntityTypeBuilder<Vendedor> builder)
+        {
+            base.Configure(builder);
+
+            builder.ToTable("VENDEDOR");
+
+            builder.Property(x => x.Nome).HasColumnName("NM_VENDEDOR").IsRequired();
+            builder.Property(x => x.Email).HasColumnName("EMAIL_VENDEDOR").IsRequired();
+
+        }
+    }
+}
